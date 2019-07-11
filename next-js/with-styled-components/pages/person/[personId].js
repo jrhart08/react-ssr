18e0f09e19e967a26045a287e4087d6d43548bd9
Joseph Hart <josephrhart@yahoo.com>
2019-07-10 22:49:55 -0700

    adding null check in sc demo

--- next-js/with-styled-components/pages/person/[personId].js
+++ next-js/with-styled-components/pages/person/[personId].js
@@ -26,12 +26,24 @@ export default () => {
 
   const person = getPerson(personId);
 
+  let content;
+  if (person) {
+    content = (
+      <>
+        <Info title="ID (from route)">{personId}</Info>
+        <Info title="First Name">{person.firstName}</Info>
+        <Info title="Last Name">{person.lastName}</Info>
+        <Info title="Character Class">{person.characterClass}</Info>
+        <Link href="/people"><a>Back</a></Link>
+      </>
+    );
+  } else {
+    content = <h1>No person found with ID {personId}</h1>
+  }
+
   return (
     <Layout>
-      <Info title="First Name">{person.firstName}</Info>
-      <Info title="Last Name">{person.lastName}</Info>
-      <Info title="Character Class">{person.characterClass}</Info>
-      <Link href="/people">Back</Link>
+      {content}
     </Layout>
   );
 };
